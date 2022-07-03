621d3090631a72b6f9ddc0c09eccdba9e954c919
Jamal Riley <73957695+jamalmriley@users.noreply.github.com>
2022-07-03 18:23:40 -0500

    Initial Commit

---
+++ Uncle John's/Uncle_John_sApp.swift
@@ -0,0 +1,17 @@
+//
+//  Uncle_John_sApp.swift
+//  Uncle John's
+//
+//  Created by Jamal Riley on 7/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Uncle_John_sApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
